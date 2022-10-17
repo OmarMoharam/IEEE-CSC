05321c0b7f6ffa925c6bd7f816da7939c5b167ba
OmarMoharam <omar.tarek8855@gmail.com>
2022-10-17 20:37:04 +0200

    Testing

---
+++ Bitwise Operator/6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int num, nth;
+    
+    printf("Enter the number: ");
+    scanf(" %d",&num);
+    printf("Enter the nth bit: ");
+    scanf(" %d",&nth);
+
+    printf("The number after toggle: %d",(1<<nth) ^ num);
+}
